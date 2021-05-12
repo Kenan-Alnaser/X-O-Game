0405f30fcd1e3e71efb33ef9d8e325cec83a31fc
Kenan Alnaser <kenan-na@live.com>
2021-05-12 12:56:45 +0200

    variables and cells

--- index.js
+++ index.js
@@ -25,7 +25,15 @@ const handleWin = (letter) => {
 
 const checkGameStatus = () => {
   // grab the cells second class from the cellDivs array and assign them to the variables (topLeft,topMiddle,topRight, middleLeft, middleMiddle,...)
-
+  const topLeft = cellDivs[0].classList[1];
+  const topMiddle = cellDivs[1].classList[1];
+  const topRight = cellDivs[2].classList[1];
+  const middleLeft = cellDivs[3].classList[1];
+  const middleMiddle = cellDivs[4].classList[1];
+  const middleRight = cellDivs[5].classList[1];
+  const bottomLeft = cellDivs[6].classList[1];
+  const bottomMiddle = cellDivs[7].classList[1];
+  const bottomRight = cellDivs[8].classList[1];
   // check winner
   if (topLeft && topLeft === topMiddle && topLeft === topRight) {
   }
